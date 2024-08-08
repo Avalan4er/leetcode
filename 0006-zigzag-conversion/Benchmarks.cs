f8892cdb948e77039ca8320863d81ce408d4b481
dorofeev <o.dorofeev@infocom-s.ru>
2024-08-08 11:09:50 +0300

    Solved 0006-zigzag-conversion

---
+++ 0006-zigzag-conversion/Benchmarks.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using Xunit.Abstractions;
+
+namespace _0006_zigzag_conversion;
+
+public class Benchmarks
+{
+    private readonly ITestOutputHelper output;
+
+    public Benchmarks(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Fact]
+    public void Run_Benchmarks()
+    {
+        var logger = new AccumulationLogger();
+
+        var config = ManualConfig.Create(DefaultConfig.Instance)
+            .AddLogger(logger)
+            .WithOptions(ConfigOptions.DisableOptimizationsValidator);
+
+        BenchmarkRunner.Run<SolutionBenchmarks>(config);
+
+        // write benchmark summary
+        output.WriteLine(logger.GetLog());
+    }
+}
---
+++ 0006-zigzag-conversion/Solution1.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace _0006_zigzag_conversion;
+
+public class Solution1
+{
+    public static string Convert(string s, int numRows)
+    {
+        if (numRows <= 1)
+            return s;
+
+        var len = s.Length;
+        if (len <= 1)
+            return s;
+
+        var result = new StringBuilder(len);
+        for (var i = 0; i < numRows; i++)
+        {
+            var cursor = i;
+            var period = numRows - 1;
+            var firstLoop = 2 * period - 2 * i;
+            var secondLoop = 2 * i;
+            while (cursor < len)
+            {
+                if (cursor >= len) continue;
+                result.Append(s[cursor]);
+                cursor += firstLoop;
+
+                if (cursor >= len) continue;
+                if (i != 0 && i != numRows - 1)
+                {
+                    result.Append(s[cursor]);
+                }
+                cursor += secondLoop;
+            }
+        }
+
+        return result.ToString();
+    }
+}
