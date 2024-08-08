544e769cf7347c0d0ad3913817a1d4d5009b0f56
Avalan4er <avalan4er@gmail.com>
2024-08-08 17:38:47 +0300

    Solved 0007-reverse-integer

---
+++ 0007-reverse-integer/Solution1.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace _0007_reverse_integer;
+
+public class Solution1
+{
+    public int Reverse(int x)
+    {
+        if (x == 0)
+            return 0;
+
+        var sign = 1;
+        if (x < 0)
+        {
+            x *= -1;
+            sign = -1;
+        }
+
+        var result = 0;
+        while (x > 0)
+        {
+            var digit = x % 10;
+            if ((int.MaxValue - 7) / 10 < result)
+                return 0;
+
+            result = result * 10 + digit;
+            x = (x - digit) / 10;
+        }
+
+        return result * sign;
+    }
+}
---
+++ 0007-reverse-integer/SolutionTests.cs
@@ -0,0 +1,54 @@
+﻿namespace _0007_reverse_integer;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution1();
+
+        var result = sln.Reverse(123);
+
+        Assert.Equal(321, result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution1();
+
+        var result = sln.Reverse(-123);
+
+        Assert.Equal(-321, result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution1();
+
+        var result = sln.Reverse(120);
+
+        Assert.Equal(21, result);
+    }
+
+    [Fact]
+    public void Case4()
+    {
+        var sln = new Solution1();
+
+        var result = sln.Reverse(1463847412);
+
+        Assert.Equal(2147483641, result);
+    }
+
+    [Fact]
+    public void Case5()
+    {
+        var sln = new Solution1();
+
+        var result = sln.Reverse(1534236469);
+
+        Assert.Equal(0, result);
+    }
+}
