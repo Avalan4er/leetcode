2d0654e4e4d8ffaeccf1ef4f76a33dc536c4a8ef
dorofeev <o.dorofeev@infocom-s.ru>
2024-08-09 10:03:53 +0300

    Solved 0009-palindrome-number

---
+++ 0009-palindrome-number/SolutionTests.cs
@@ -0,0 +1,34 @@
+﻿namespace _0009_palindrome_number;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsPalindrome(121);
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsPalindrome(-121);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsPalindrome(10);
+
+        Assert.False(result);
+    }
+}
