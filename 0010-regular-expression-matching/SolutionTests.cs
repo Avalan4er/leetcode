31841b57ec38c0901083d3bf23c308af29f9b88e
Avalan4er <avalan4er@gmail.com>
2024-08-09 18:07:49 +0300

    Attempt at 0010-regular-expression-match

---
+++ 0010-regular-expression-matching/SolutionTests.cs
@@ -0,0 +1,104 @@
+﻿namespace _0010_regular_expression_matching;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("aa", "a");
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("aa", "a*");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("ab", ".*");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case4()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("aaaabaaaabbbb", "a*ba*b*");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case5()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("aaaabaaaabbbb", "a*b.*");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case6()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("aaaabaaaabbbb", "a*.a*b*");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case7()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("aab", "c*a*b");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case8()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("aaaabaaaa", "a.*b*.*a*");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case9()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("ababbbba", "a.*b*.*a*a");
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Case10()
+    {
+        var sln = new Solution1();
+
+        var result = sln.IsMatch("ababbbb", "a.*b*.*a*a");
+
+        Assert.False(result);
+    }
+}
