6d0dda06c414b562f2d704e76908428983cf3682
Avalan4er <avalan4er@gmail.com>
2024-08-12 16:36:24 +0300

    Solved 0012-integer-to-roman

---
+++ 0012-integer-to-roman/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace _0012_integer_to_roman;
+
+public class Solution
+{
+    public string IntToRoman(int num)
+    {
+        return IntToRoman(num, 10);
+    }
+
+    private string IntToRoman(int num, int pos)
+    {
+        var digit = num % pos;
+
+        var t = digit;
+        var sb = new StringBuilder();
+        if (num * 10 >= pos * 10)
+            sb.Append(IntToRoman(num - digit, pos * 10));
+
+        while (t > 0)
+        {
+            var sub = t switch
+            {
+                >= 1000 => 1000,
+                900 => 900,
+                >= 500 => 500,
+                400 => 400,
+                >= 100 => 100,
+                90 => 90,
+                >= 50 => 50,
+                40 => 40,
+                >= 10 => 10,
+                9 => 9,
+                >= 5 => 5,
+                4 => 4,
+                >= 1 => 1,
+                _ => throw new Exception()
+            };
+
+            t -= sub;
+            sb.Append(sub switch
+            {
+                1000 => 'M',
+                900 => "CM",
+                500 => 'D',
+                400 => "CD",
+                100 => 'C',
+                90 => "XC",
+                50 => 'L',
+                40 => "XL",
+                10 => 'X',
+                9 => "IX",
+                5 => 'V',
+                4 => "IV",
+                1 => 'I',
+                _ => throw new Exception()
+            });
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ 0012-integer-to-roman/SolutionTests.cs
@@ -0,0 +1,44 @@
+﻿namespace _0012_integer_to_roman;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        var result = sln.IntToRoman(3749);
+
+        Assert.Equal("MMMDCCXLIX", result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.IntToRoman(58);
+
+        Assert.Equal("LVIII", result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+
+        var result = sln.IntToRoman(1994);
+
+        Assert.Equal("MCMXCIV", result);
+    }
+
+    [Fact]
+    public void Case4()
+    {
+        var sln = new Solution();
+
+        var result = sln.IntToRoman(10);
+
+        Assert.Equal("X", result);
+    }
+}
