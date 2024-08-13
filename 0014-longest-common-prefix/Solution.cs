7045e57b95298ffc6ccb796fc6f25ff19a6b10e7
Avalan4er <avalan4er@gmail.com>
2024-08-13 10:02:32 +0300

    Solved 0014-longest-common-prefix

---
+++ 0014-longest-common-prefix/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace _0014_longest_common_prefix;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0)
+            return string.Empty;
+
+        var sb = new StringBuilder();
+        var cursor = 0;
+        var finished = false;
+        while (!finished)
+        {
+            char? curChar = null;
+            for (var i = 0; i < strs.Length; i++)
+            {
+                var curString = strs[i];
+                if (cursor >= curString.Length)
+                {
+                    finished = true;
+                    break;
+                }
+
+                if (i == 0)
+                {
+                    curChar = curString[cursor];
+                    continue;
+                }
+
+                if (curChar != curString[cursor])
+                {
+                    finished = true;
+                    break;
+                }
+            }
+
+            cursor++;
+            if (!finished)
+                sb.Append(curChar);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ 0014-longest-common-prefix/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace _0014_longest_common_prefix;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        var result = sln.LongestCommonPrefix(["flower", "flow", "flight"]);
+
+        Assert.Equal("fl", result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.LongestCommonPrefix(["dog", "racecar", "car"]);
+
+        Assert.Equal("", result);
+    }
+}
