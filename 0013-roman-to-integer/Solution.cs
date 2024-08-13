e6852946232405ab1c43da7bc050aa1af80fd6ad
Avalan4er <avalan4er@gmail.com>
2024-08-13 09:44:06 +0300

    Solved 0013-roman-to-integer

---
+++ 0013-roman-to-integer/Solution.cs
@@ -0,0 +1,62 @@
+﻿namespace _0013_roman_to_integer;
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        var chars = s.AsSpan();
+        var cursor = chars.Length - 1;
+        var result = 0;
+
+        while (cursor >= 0)
+        {
+            var c = chars[cursor];
+            var num = c switch
+            {
+                'M' => 1000,
+                'D' => 500,
+                'C' => 100,
+                'L' => 50,
+                'X' => 10,
+                'V' => 5,
+                'I' => 1,
+                _ => throw new Exception()
+            };
+            result += num;
+            cursor--;
+            if (cursor < 0)
+                break;
+
+            var nextChar = chars[cursor];
+            switch (c)
+            {
+                case 'V':
+                case 'X':
+                    if (nextChar == 'I')
+                    {
+                        result--;
+                        cursor--;
+                    }
+                    break;
+                case 'L':
+                case 'C':
+                    if (nextChar == 'X')
+                    {
+                        result -= 10;
+                        cursor--;
+                    }
+                    break;
+                case 'D':
+                case 'M':
+                    if (nextChar == 'C')
+                    {
+                        result -= 100;
+                        cursor--;
+                    }
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ 0013-roman-to-integer/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿namespace _0013_roman_to_integer;
+
+public class SolutionTests
+{
+    [Theory]
+    [InlineData("III", 3)]
+    [InlineData("LVIII", 58)]
+    [InlineData("MCMXCIV", 1994)]
+    public void Case1(string input, int output)
+    {
+        var sln = new Solution();
+
+        var result = sln.RomanToInt(input);
+
+        Assert.Equal(output, result);
+    }
+}
