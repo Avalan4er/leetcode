866fd2d499a0fdc4b8af2b70af65bc38e9374c39
Avalan4er <avalan4er@gmail.com>
2024-08-14 12:41:45 +0300

    Solved 0015-3sum

---
+++ 0015-3sum/Solution.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _0015_3sum;
+
+public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var data = nums.AsSpan();
+        data.Sort((x, y) => x <= y ? 1 : -1);
+        var result = new List<IList<int>>();
+
+        var start = 0;
+        while (start < data.Length - 2)
+        {
+            int middle = start + 1, end = data.Length - 1;
+            var x = data[start];
+            while (middle < end)
+            {
+                int y = data[middle], z = data[end];
+                if (x + y + z == 0)
+                {
+                    result.Add([x, y, z]);
+
+                    while (middle < end && data[middle] == y)
+                        middle++;
+                    while (middle < end && data[end] == z)
+                        end--;
+                }
+                else if (x + y + z > 0)
+                {
+                    middle++;
+                }
+                else if (x + y + z < 0)
+                {
+                    end--;
+                }
+            }
+
+            while (start < data.Length - 1 && data[start] == x)
+                start++;
+        }
+
+        return result;
+    }
+}
+
+public class IListComparer : IEqualityComparer<IList<int>>
+{
+    public bool Equals(IList<int>? x, IList<int>? y)
+    {
+        if (x is null || y is null)
+            return false;
+
+        return x.SequenceEqual(y);
+    }
+
+    public int GetHashCode([DisallowNull] IList<int> obj)
+    {
+        return obj[0] * 10512 +
+            obj[1] * 10512 +
+            obj[2] * 10512;
+    }
+}
---
+++ 0015-3sum/SolutionTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _0015_3sum;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        var result = sln.ThreeSum([-1, 0, 1, 2, -1, -4]);
+
+        Assert.True(result.SequenceEqual([[2, -1, -1], [1, 0, -1]], new IListComparer()));
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.ThreeSum([0, 1, 1]);
+
+        Assert.True(result.SequenceEqual([], new IListComparer()));
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+
+        var result = sln.ThreeSum([0, 0, 0]);
+
+        Assert.True(result.SequenceEqual([[0, 0, 0]], new IListComparer()));
+    }
+
+    [Fact]
+    public void Case4()
+    {
+        var sln = new Solution();
+
+        var input = Enumerable.Range(0, 3000).Select(_ => Random.Shared.Next(-1000, 1000)).ToArray();
+        var result = sln.ThreeSum(input);
+
+        var except = result.Any();
+        Assert.True(except);
+    }
+}
