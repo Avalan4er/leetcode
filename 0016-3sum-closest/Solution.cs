18ee43c0245c22552b5ef48f1b9b5981f0e5fe6e
Avalan4er <avalan4er@gmail.com>
2024-08-14 15:57:58 +0300

    Solved 0016-3sum-closest

---
+++ 0016-3sum-closest/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace _0016_3sum_closest;
+
+public class Solution
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        var data = nums.AsSpan();
+        data.Sort((x, y) => x <= y ? 1 : -1);
+
+        var closest = int.MaxValue;
+        var start = 0;
+        while (start < data.Length - 2)
+        {
+            var x = data[start];
+            int mid = start + 1, end = data.Length - 1;
+            while (mid < end)
+            {
+                int y = data[mid], z = data[end];
+                var value = x + y + z;
+
+                if (value == target)
+                    return target;
+                if (Math.Abs(target - closest) > Math.Abs(target - value))
+                {
+                    closest = value;
+                }
+                if (target - value > 0)
+                {
+                    end--;
+                }
+                else
+                {
+                    mid++;
+                }
+            }
+
+            while (start < data.Length - 2 && data[start] == x)
+                start++;
+        }
+
+        return closest;
+    }
+}
---
+++ 0016-3sum-closest/SolutionTests.cs
@@ -0,0 +1,35 @@
+﻿namespace _0016_3sum_closest;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        var result = sln.ThreeSumClosest([-1, 2, 1, -4], 1);
+
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.ThreeSumClosest([0, 0, 0], 1);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+
+        var data = Enumerable.Range(0, 500).Select(_ => Random.Shared.Next(-1000, 1000)).ToArray();
+        var result = sln.ThreeSumClosest(data, 1);
+
+        Assert.Equal(1, result);
+    }
+}
