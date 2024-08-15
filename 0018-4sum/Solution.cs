423117f0d6f55a742f04da766e5013c08adba88c
Avalan4er <avalan4er@gmail.com>
2024-08-15 09:48:42 +0300

    Solved 0018-4sum

---
+++ 0018-4sum/Solution.cs
@@ -0,0 +1,60 @@
+﻿using Xunit.Sdk;
+
+namespace _0018_4sum;
+
+public class Solution
+{
+    public IList<IList<int>> FourSum(int[] nums, int target)
+    {
+        var data = nums.AsSpan();
+        data.Sort((x, y) => x > y ? -1 : 1);
+        var one = 0;
+
+        var result = new List<IList<int>>();
+        while (one < data.Length - 3)
+        {
+            var x = data[one];
+
+            var four = data.Length - 1;
+            while (four > one + 2)
+            {
+                var u = data[four];
+                var two = one + 1;
+                var three = four - 1;
+
+                while (three > two)
+                {
+                    var y = data[two];
+                    var z = data[three];
+
+                    var value = (long)x + y + z + u;
+                    if (value == target)
+                    {
+                        result.Add([x, y, z, u]);
+
+                        while (three > two && data[two] == y)
+                            two++;
+                        while (three > two && data[three] == z)
+                            three--;
+                    }
+                    else if (value > target)
+                    {
+                        two++;
+                    }
+                    else if (value < target)
+                    {
+                        three--;
+                    }
+                }
+
+                while (four > one + 2 && data[four] == u)
+                    four--;
+            }
+
+            while (one < data.Length - 3 && data[one] == x)
+                one++;
+        }
+
+        return result;
+    }
+}
---
+++ 0018-4sum/SolutionTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _0018_4sum;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        var result = sln.FourSum([1, 0, -1, 0, -2, 2], 0);
+
+        Assert.True(result.SequenceEqual([[2, 1, -1, -2], [2, 0, 0, -2], [1, 0, 0, -1]], new IListComparer()));
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.FourSum([2, 2, 2, 2, 2], 8);
+
+        Assert.True(result.SequenceEqual([[2, 2, 2, 2]], new IListComparer()));
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+
+        var result = sln.FourSum([1000000000, 1000000000, 1000000000, 1000000000], -294967296);
+
+        Assert.True(result.SequenceEqual([], new IListComparer()));
+    }
+
+    class IListComparer : IEqualityComparer<IList<int>>
+    {
+        public bool Equals(IList<int>? x, IList<int>? y)
+        {
+            if (x is null || y is null)
+                return false;
+
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode([DisallowNull] IList<int> obj)
+        {
+            return obj[0] * 12512836
+                + obj[1] * 12512836
+                + obj[2] * 12512836
+                + obj[3] * 12512836;
+        }
+    }
+}
