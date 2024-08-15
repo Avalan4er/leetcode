9094ac013095a9b889fd653c2ff0cde13babe323
Avalan4er <avalan4er@gmail.com>
2024-08-15 12:27:43 +0300

    Solved 0020-valid-parentheses

---
+++ 0020-valid-parentheses/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace _0020_valid_parentheses;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        var pairs = new Dictionary<char, char>
+        {
+            ['('] = ')',
+            ['{'] = '}',
+            ['['] = ']',
+        };
+
+        foreach (var c in s.AsSpan())
+        {
+            switch (c)
+            {
+                case '{':
+                case '(':
+                case '[':
+                    stack.Push(c);
+                    break;
+                case '}':
+                case ')':
+                case ']':
+                    if (!stack.TryPop(out var latest) || pairs[latest] != c)
+                        return false;
+                    break;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
---
+++ 0020-valid-parentheses/SolutionTests.cs
@@ -0,0 +1,17 @@
+﻿namespace _0020_valid_parentheses;
+
+public class SolutionTests
+{
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("(]", false)]
+    public void Case(string str, bool expected)
+    {
+        var sln = new Solution();
+
+        var result = sln.IsValid(str);
+
+        Assert.Equal(expected, result);
+    }
+}
