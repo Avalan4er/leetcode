d6cce2308a5a458214b8d0bc3638dc6090ba8c8a
Avalan4er <avalan4er@gmail.com>
2024-08-15 15:26:43 +0300

    Solved 0021-merge-two-sorted-lists

---
+++ 0021-merge-two-sorted-lists/Solution.cs
@@ -0,0 +1,69 @@
+﻿namespace _0021_merge_two_sorted_lists;
+
+public class ListNode(int val = 0, ListNode? next = null)
+{
+    public int val = val;
+    public ListNode? next = next;
+
+    public override int GetHashCode()
+    {
+        var hash = val * 21951266;
+
+        if (next is not null)
+            hash += next.GetHashCode() * 1251256;
+
+        return hash;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not ListNode node)
+            return false;
+        return GetHashCode() == node.GetHashCode();
+    }
+}
+
+public class Solution
+{
+    public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2)
+    {
+        if (list1 is null && list2 is null)
+            return null;
+        if (list1 is null || list2 is null)
+            return list1 ?? list2;
+
+        ListNode head;
+        if (list1.val < list2.val)
+        {
+            head = new(list1.val);
+            list1 = list1.next;
+        }
+        else
+        {
+            head = new(list2.val);
+            list2 = list2.next;
+        }
+
+        var current = head;
+        while (list1 is not null && list2 is not null)
+        {
+            if (list1.val < list2.val)
+            {
+                current.next = new(list1.val);
+                current = current.next;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = new(list2.val);
+                current = current.next;
+                list2 = list2.next;
+            }
+        }
+
+        var tail = list1 ?? list2;
+        current.next = tail;
+
+        return head;
+    }
+}
---
+++ 0021-merge-two-sorted-lists/SolutionTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic;
+
+namespace _0021_merge_two_sorted_lists;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        var result = sln.MergeTwoLists(
+            CreateList([1, 2, 4]),
+            CreateList([1, 3, 4]));
+
+        Assert.Equal(CreateList([1, 1, 2, 3, 4, 4]), result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.MergeTwoLists(null, null);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+
+        var result = sln.MergeTwoLists(null, CreateList([0]));
+
+        Assert.Equal(CreateList([0]), result);
+    }
+
+    private static ListNode CreateList(ReadOnlySpan<int> numbers)
+    {
+        var current = new ListNode(numbers[0]);
+        var head = current;
+        var idx = 1;
+        while (idx < numbers.Length)
+        {
+            current.next = new(numbers[idx]);
+            current = current.next;
+            idx++;
+        }
+
+        return head;
+    }
+}
