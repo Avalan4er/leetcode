3dd1cce38c9ecefc32769e2b652ffa2b53302587
Avalan4er <avalan4er@gmail.com>
2024-08-15 11:50:20 +0300

    Solved 0019-remove-nth-node-from-end-of-list

---
+++ 0019-remove-nth-node-from-end-of-list/Solution.cs
@@ -0,0 +1,62 @@
+﻿namespace _0019_remove_nth_node_from_end_of_list;
+
+public class ListNode
+{
+    public int val;
+    public ListNode? next;
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+
+        if (obj is not ListNode node)
+            return false;
+
+        return GetHashCode() == node.GetHashCode();
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = val * 125166326;
+
+        if (next is not null)
+            hash += next.GetHashCode();
+
+        return hash;
+    }
+}
+
+public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var removed = Remove(head, n, 0);
+        if (removed == -1)
+            return head.next!;
+        if (removed is null)
+            throw new Exception();
+        return head;
+    }
+
+    public int? Remove(ListNode current, int n, int depth)
+    {
+        int? nodeIdxToRemove;
+        if (current.next is null)
+            nodeIdxToRemove = depth - n;
+        else
+            nodeIdxToRemove = Remove(current.next, n, depth + 1);
+
+        if (nodeIdxToRemove != null && nodeIdxToRemove == depth)
+        {
+            current.next = current.next!.next;
+        }
+
+        return nodeIdxToRemove;
+    }
+}
---
+++ 0019-remove-nth-node-from-end-of-list/SolutionTests.cs
@@ -0,0 +1,38 @@
+﻿namespace _0019_remove_nth_node_from_end_of_list;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+        var head = new ListNode(1, new(2, new(3, new(4, new(5)))));
+
+        var result = sln.RemoveNthFromEnd(head, 2);
+
+        var expected = new ListNode(1, new(2, new(3, new(5))));
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+        var head = new ListNode(1);
+
+        var result = sln.RemoveNthFromEnd(head, 1);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+        var head = new ListNode(1, new(2));
+
+        var result = sln.RemoveNthFromEnd(head, 1);
+
+        Assert.Equal(new(1), result);
+    }
+}
