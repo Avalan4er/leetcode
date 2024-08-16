cd26d48c84ea3633a3c06c08bf1ca4eea88807c2
Avalan4er <avalan4er@gmail.com>
2024-08-16 11:46:17 +0300

    Solved 0022-generate-parentheses

---
+++ 0022-generate-parentheses/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace _0022_generate_parentheses;
+
+public class Solution
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var results = Generate(n, [], 0, 0);
+        return results.Select(x => string.Join(string.Empty, x)).ToList();
+    }
+
+    public IEnumerable<char[]> Generate(int n, char[] buffer, int position, int opened)
+    {
+        if (position == 2 * n)
+            yield return buffer;
+        else
+        {
+            if (opened < n && 2 * n - position > opened)
+            {
+                var open = new char[position + 1];
+                Array.Copy(buffer, open, position);
+                open[position] = '(';
+                foreach (var result in Generate(n, open, position + 1, opened + 1))
+                    yield return result;
+            }
+
+            if (opened > 0)
+            {
+                var close = new char[position + 1];
+                Array.Copy(buffer, close, position);
+                close[position] = ')';
+
+                foreach (var result in Generate(n, close, position + 1, opened - 1))
+                    yield return result;
+            }
+        }
+    }
+}
---
+++ 0022-generate-parentheses/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿namespace _0022_generate_parentheses;
+
+public class SolutionTest
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        var result = sln.GenerateParenthesis(3);
+
+        Assert.Equal(["((()))", "(()())", "(())()", "()(())", "()()()"], result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.GenerateParenthesis(1);
+
+        Assert.Equal(["()"], result);
+    }
+}
