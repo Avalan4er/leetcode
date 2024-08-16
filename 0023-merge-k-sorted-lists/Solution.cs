61e71b962c3f96fabd8f7c1ac329e04d5e4cae7a
Avalan4er <avalan4er@gmail.com>
2024-08-16 15:02:58 +0300

    Solved 0023-merge-k-sorted-lists

---
+++ 0023-merge-k-sorted-lists/Solution.cs
@@ -0,0 +1,53 @@
+﻿
+namespace _0023_merge_k_sorted_lists;
+
+public class Solution
+{
+    public ListNode? MergeKLists(ListNode?[]? lists)
+    {
+        if (lists is null)
+            return null;
+
+        var (head, headIdx) = Min(lists);
+        if (head is null)
+            return null;
+        lists[headIdx] = head.next;
+
+        var current = head;
+        while (true)
+        {
+            var (min, minIdx) = Min(lists);
+            if (min is null)
+                break;
+            current!.next = min;
+            current = min;
+            lists[minIdx] = min.next;
+        }
+
+        return head;
+    }
+
+    private static (ListNode? node, int idx) Min(ListNode?[] lists)
+    {
+        if (lists.Length == 0)
+            return (null, 0);
+
+        var minimal = lists[0];
+        var minimalIdx = 0;
+
+        for (var i = 1; i < lists.Length; i++)
+        {
+            var current = lists[i];
+            if (current is null)
+                continue;
+
+            if (minimal is null || minimal.val > current.val)
+            {
+                minimal = current;
+                minimalIdx = i;
+            }
+        }
+
+        return (minimal, minimalIdx);
+    }
+}
---
+++ 0023-merge-k-sorted-lists/SolutionTests.cs
@@ -0,0 +1,49 @@
+﻿namespace _0023_merge_k_sorted_lists;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+        ListNode[] lists = [
+            ListNode.New([1, 4, 5]),
+            ListNode.New([1, 3, 4]),
+            ListNode.New([2, 6])];
+
+        var result = sln.MergeKLists(lists);
+
+        var expected = ListNode.New([1, 1, 2, 3, 4, 4, 5, 6]);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        var result = sln.MergeKLists([]);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+
+        var result = sln.MergeKLists(null);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Case4()
+    {
+        var sln = new Solution();
+
+        var result = sln.MergeKLists([null]);
+
+        Assert.Null(result);
+    }
+}
