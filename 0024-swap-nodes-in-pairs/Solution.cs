bd553806b1c658a15e76a85e783564ce78d2045c
Avalan4er <avalan4er@gmail.com>
2024-08-19 10:05:01 +0300

    Solved 0024-swap-nodes-in-pairs

---
+++ 0024-swap-nodes-in-pairs/Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace _0024_swap_nodes_in_pairs;
+
+public class Solution
+{
+    public ListNode? SwapPairs(ListNode? head)
+    {
+        if (head is null)
+            return null;
+
+        ListNode? current = head;
+        ListNode? next = head.next;
+        ListNode? result = null;
+        ListNode? newHead = null;
+        while (current is not null)
+        {
+            if (next is null)
+            {
+                if (result is null)
+                {
+                    result = new(current.val);
+                    newHead = result;
+                }
+                else
+                {
+                    result.next = new(current.val);
+                    result = result.next;
+                }
+            }
+            else
+            {
+                if (result is null)
+                {
+                    result = new(next.val, new(current.val));
+                    newHead = result;
+                    result = result.next;
+                }
+                else
+                {
+                    result.next = new(next.val, new(current.val));
+                    result = result.next.next;
+                }
+            }
+
+            current = next?.next;
+            next = current?.next;
+        }
+
+        return newHead;
+    }
+}
---
+++ 0024-swap-nodes-in-pairs/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿namespace _0024_swap_nodes_in_pairs;
+
+public class SolutionTest
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+        var result = sln.SwapPairs(ListNode.New([1, 2, 3, 4]));
+        Assert.Equal(ListNode.New([2, 1, 4, 3]), result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+        var result = sln.SwapPairs(null);
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+        var result = sln.SwapPairs(ListNode.New([1]));
+        Assert.Equal(ListNode.New([1]), result);
+    }
+}
