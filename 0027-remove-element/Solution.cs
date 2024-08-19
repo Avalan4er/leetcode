f5e21cee3bade9f1757d87492ead5eacf9010761
Avalan4er <avalan4er@gmail.com>
2024-08-19 16:49:13 +0300

    Solved 0027-remove-element

---
+++ 0027-remove-element/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace _0027_remove_element;
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var data = nums.AsSpan();
+
+        var count = 0;
+        for (var i = 0; i < data.Length; i++)
+        {
+            if (data[i] == val)
+                continue;
+
+            data[count] = data[i];
+            count++;
+        }
+
+        return count;
+    }
+}
---
+++ 0027-remove-element/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace _0027_remove_element;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        int[] data = [3, 2, 2, 3];
+        var result = sln.RemoveElement(data, 3);
+
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        int[] data = [0, 1, 2, 2, 3, 0, 4, 2];
+        var result = sln.RemoveElement(data, 2);
+
+        Assert.Equal(5, result);
+    }
+}
