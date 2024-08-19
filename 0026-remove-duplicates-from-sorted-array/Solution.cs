034dd19a316de45570a107368a194d3f0ab2b9e5
Avalan4er <avalan4er@gmail.com>
2024-08-19 16:42:28 +0300

    Solved 0026-remove-duplicates-from-sorted-array

---
+++ 0026-remove-duplicates-from-sorted-array/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace _0026_remove_duplicates_from_sorted_array;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var data = nums.AsSpan();
+
+        var index = 0;
+        for (var i = 1; i < data.Length; i++)
+            if (data[index] != data[i])
+                data[++index] = data[i];
+
+        return ++index;
+    }
+}
---
+++ 0026-remove-duplicates-from-sorted-array/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace _0026_remove_duplicates_from_sorted_array;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        int[] data = [1, 1, 2];
+        var result = sln.RemoveDuplicates(data);
+
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        int[] data = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+        var result = sln.RemoveDuplicates(data);
+
+        Assert.Equal(5, result);
+    }
+}
