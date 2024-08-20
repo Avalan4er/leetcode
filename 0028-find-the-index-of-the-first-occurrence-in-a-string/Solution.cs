5735183618d21edb311db80286b79a64fe3d189b
Avalan4er <avalan4er@gmail.com>
2024-08-20 09:30:47 +0300

    Solved 0028-find-the-index-of-the-first-occurrence-in-a-string

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace _0028_find_the_index_of_the_first_occurrence_in_a_string;
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        var data = haystack.AsSpan();
+        var dataLength = data.Length;
+        var keyword = needle.AsSpan();
+        var keywordLength = keyword.Length;
+
+        var result = -1;
+        for (var i = 0; i < dataLength; i++)
+        {
+            if (keyword[0] == data[i])
+            {
+                result = i;
+                for (var j = 0; j < keywordLength && i + j < dataLength; j++)
+                {
+                    if (keyword[j] != data[i + j])
+                        break;
+
+                    if (j == keywordLength - 1)
+                        return result;
+                }
+                result = -1;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/SolutionTests.cs
@@ -0,0 +1,19 @@
+﻿namespace _0028_find_the_index_of_the_first_occurrence_in_a_string;
+
+public class SolutionTests
+{
+    [Theory]
+    [InlineData("sadbutsad", "sad", 0)]
+    [InlineData("leetcode", "leeto", -1)]
+    [InlineData("aaa", "aaaa", -1)]
+    [InlineData("mississippi", "issip", 4)]
+    [InlineData("a", "a", 0)]
+    public void Case1(string haystack, string needle, int expected)
+    {
+        var sln = new Solution();
+
+        var result = sln.StrStr(haystack, needle);
+
+        Assert.Equal(expected, result);
+    }
+}
