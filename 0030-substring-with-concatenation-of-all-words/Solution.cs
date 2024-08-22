b339a3e4b433c4b47d428668f81fb251fa81e183
Avalan4er <avalan4er@gmail.com>
2024-08-22 15:00:39 +0300

    Solved 0030-substring-with-concatenation-of-all-words

---
+++ 0030-substring-with-concatenation-of-all-words/Solution.cs
@@ -0,0 +1,60 @@
+﻿namespace _0030_substring_with_concatenation_of_all_words;
+
+public class Solution
+{
+    public IList<int> FindSubstring(string s, string[] words)
+    {
+        if (string.IsNullOrEmpty(s))
+            return [];
+        if (words.Length == 0)
+            return [];
+
+        var searchMap = CreateSearchMap(words);
+        var wLen = words[0].Length;
+        var sLen = s.Length;
+        var data = s.AsSpan();
+        var wCount = words.Length;
+        var substrLen = wLen * wCount;
+
+        var result = new List<int>();
+        for (var i = 0; i < sLen - substrLen + 1; i++)
+        {
+            var map = Search(data.Slice(i, substrLen), wLen, searchMap);
+            if (map is null) continue;
+            if (searchMap.SequenceEqual(map))
+                result.Add(i);
+        }
+
+        return result;
+    }
+
+    private static SortedDictionary<string, int> CreateSearchMap(string[] words)
+    {
+        var result = new SortedDictionary<string, int>();
+
+        foreach (var word in words)
+        {
+            if (!result.ContainsKey(word))
+                result[word] = 0;
+            result[word]++;
+        }
+
+        return result;
+    }
+
+    private static Dictionary<string, int>? Search(ReadOnlySpan<char> input, int wordLength, SortedDictionary<string, int> searchMap)
+    {
+        var result = searchMap.ToDictionary(x => x.Key, x => 0);
+        var inputLength = input.Length;
+
+        for (var i = 0; i < inputLength; i += wordLength)
+        {
+            var word = input.Slice(i, wordLength).ToString();
+            if (!result.TryGetValue(word, out var value))
+                return null;
+            result[word] = ++value;
+        }
+
+        return result;
+    }
+}
---
+++ 0030-substring-with-concatenation-of-all-words/Solution1.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _0030_substring_with_concatenation_of_all_words;
+
+public class Solution1
+{
+    public IList<int> FindSubstring(string s, string[] words)
+    {
+        if (string.IsNullOrEmpty(s))
+            return [];
+        if (words.Length == 0)
+            return [];
+
+        var index = 0;
+        var sLen = s.Length;
+        var result = new List<int>();
+        var chainLength = words.Length * words[0].Length - 1;
+        while (index < sLen - chainLength)
+        {
+            var chainIdx = CheckChain(s, index, words);
+            if (chainIdx != -1)
+                result.Add(index);
+            index++;
+        }
+
+        return result;
+    }
+
+    private static int CheckChain(ReadOnlySpan<char> s, int start, string[] words)
+    {
+        var search = new List<string>(words);
+        var chainIndex = start;
+        var wordLen = words[0].Length;
+        while (search.Count > 0 && chainIndex < s.Length)
+        {
+            var substring = s.Slice(chainIndex, wordLen);
+            var wordIdx = FindWord(substring, search);
+            if (wordIdx != -1)
+            {
+                var word = search[wordIdx];
+                search.RemoveAt(wordIdx);
+                chainIndex += word.Length;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        return search.Count > 0 ? -1 : start;
+    }
+
+    private static int FindWord(ReadOnlySpan<char> s, List<string> words)
+    {
+        for (var i = 0; i < words.Count; i++)
+        {
+            var word = words[i];
+            if (s.SequenceEqual(word))
+                return i;
+        }
+
+        return -1;
+    }
+}
