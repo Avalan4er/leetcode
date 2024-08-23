052d0193655e703c04466f6eb2a8fc9f7816aacd
Avalan4er <avalan4er@gmail.com>
2024-08-23 15:20:45 +0300

    Solved 0031-next-permutation

---
+++ 0031-next-permutation/Solution.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace _0031_next_permutation;
+
+public class Solution
+{
+    public void NextPermutation(int[] nums)
+    {
+        var data = nums.AsSpan();
+        var permIdx = GetPermutationIdx(nums);
+        if (permIdx == 0)
+        {
+            Array.Sort(nums);
+        }
+        else
+        {
+            var slice = data[permIdx..];
+            var minIdx = permIdx + FindNextMin(slice, nums[permIdx - 1]);
+            Swap(data, permIdx - 1, minIdx);
+            slice.Sort();
+        }
+    }
+
+    private static int GetPermutationIdx(ReadOnlySpan<int> nums)
+    {
+        var len = nums.Length;
+        for (var i = len - 1; i > 0; i--)
+        {
+            var current = nums[i];
+            var prev = nums[i - 1];
+
+            if (prev < current)
+                return i;
+        }
+
+        return 0;
+    }
+
+    private static int FindNextMin(ReadOnlySpan<int> nums, int lowerLimit)
+    {
+        var idx = 0;
+        var min = nums[idx];
+        var len = nums.Length;
+        for (var i = 0; i < len; i++)
+        {
+            var item = nums[i];
+            if (item < min && item > lowerLimit)
+            {
+                min = item;
+                idx = i;
+            }
+        }
+
+        return idx;
+    }
+
+    private static void Swap(Span<int> nums, int i, int j)
+    {
+        (nums[i], nums[j]) = (nums[j], nums[i]);
+    }
+}
---
+++ 0031-next-permutation/SolutionTests.cs
@@ -0,0 +1,81 @@
+﻿namespace _0031_next_permutation;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Case1()
+    {
+        var sln = new Solution();
+
+        int[] input = [1, 2, 3];
+        sln.NextPermutation(input);
+
+        Assert.Equal([1, 3, 2], input);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var sln = new Solution();
+
+        int[] input = [3, 2, 1];
+        sln.NextPermutation(input);
+
+        Assert.Equal([1, 2, 3], input);
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var sln = new Solution();
+
+        int[] input = [1, 1, 5];
+        sln.NextPermutation(input);
+
+        Assert.Equal([1, 5, 1], input);
+    }
+
+    [Fact]
+    public void Case4()
+    {
+        var sln = new Solution();
+
+        int[] input = [1, 3, 2];
+        sln.NextPermutation(input);
+
+        Assert.Equal([2, 1, 3], input);
+    }
+
+    [Fact]
+    public void Case5()
+    {
+        var sln = new Solution();
+
+        int[] input = [2, 3, 1];
+        sln.NextPermutation(input);
+
+        Assert.Equal([3, 1, 2], input);
+    }
+
+    [Fact]
+    public void Case6()
+    {
+        var sln = new Solution();
+
+        int[] input = [3, 1, 2];
+        sln.NextPermutation(input);
+
+        Assert.Equal([3, 2, 1], input);
+    }
+
+    [Fact]
+    public void Case7()
+    {
+        var sln = new Solution();
+
+        int[] input = [1, 5, 1];
+        sln.NextPermutation(input);
+
+        Assert.Equal([5, 1, 1], input);
+    }
+}
