8afb8890a92222780b92584a9389639af1467ed7
Oleg Dorofeev <avalan4er@gmail.com>
2025-05-02 16:10:01 +0000

    new proj

---
+++ code-camp/004-knight-move/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+var n = 31;
+var m = 34;
+var f = Fact(n + m) / (Fact(n + m) - Fact(m));
+
+Console.WriteLine(f);
+
+int Fact(int x)
+{
+    return Enumerable.Range(1, x).Aggregate((acc, x) => acc * x);
+}
